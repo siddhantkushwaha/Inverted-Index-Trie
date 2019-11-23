bfc4ae4418f18f9ecfd25ee66175623efddac56e
siddhant <k16.siddhant@gmail.com>
2019-11-24 00:08:23 +0530

    final changes

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory, url_for
+
+from urllib.parse import urlparse
 
 from pytrie import trie
 from main import index_file
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+
+def create_link(doc_id):
+    uri = urlparse(request.url)
+    url = '{uri.scheme}://{uri.netloc}/'.format(uri=uri)
+
+    if doc_id.endswith('.pdf'):
+        return f'{url}files/{doc_id}'
+
+    idx = doc_id.find('.txt')
+    if idx > 1:
+        return f'{url}files/{doc_id[:idx+4]}'
+
+    return ''
 
 
 @app.route('/')
@@ -16,7 +32,7 @@ def home():
 @app.route('/index', methods=['POST'])
 def index():
     file_buf = request.files['file']
-    
+
     file_name = file_buf.filename
     index_file(inv_idx, file_buf, file_name)
 
@@ -26,6 +42,7 @@ def index():
 
     return f'Indexed. {file_name}\n'
 
+
 @app.route('/word_search', methods=['GET'])
 def word_search():
     query = request.args.get('query', None)
@@ -33,11 +50,12 @@ def word_search():
 
     results = []
     if query is not None:
-        results = inv_idx.search_word(query)
-        results = list(results)
+        docs = inv_idx.search_word(query)
+        results = [(doc[0], doc[1], create_link(doc[0])) for doc in docs]
 
     print(results)
     return jsonify(results)
+
 
 @app.route('/prefix_search', methods=['GET'])
 def prefix_search():
@@ -48,18 +66,29 @@ def prefix_search():
     if query is not None:
         res = inv_idx.get_by_prefix(query)
         for word, docs in res:
-            results.append((word, list(docs)))
+            results.append(
+                (word, [(doc[0], doc[1], create_link(doc[0])) for doc in docs]))
 
     print(results)
     return jsonify(results)
+
 
 @app.route('/clear', methods=['GET'])
 def clear():
     # delete the old trie and declare a new one.
     global inv_idx
     del inv_idx
+
+    # clear uploaded_files directory
+    os.system('rm uploaded_files/*')
+
     inv_idx = trie()
     return 'Index cleared.'
+
+
+@app.route('/files/<path:path>')
+def send_file(path):
+    return send_from_directory('uploaded_files', path)
 
 
 if __name__ == '__main__':
