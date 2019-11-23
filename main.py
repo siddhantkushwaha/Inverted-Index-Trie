0481a1a1650513bd20d81b1648041af87c2dbe67
siddhant <k16.siddhant@gmail.com>
2019-11-23 22:22:49 +0530

    pdf is one doc

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 import os
 import re
-from pprint import pprint
 
 import pdftotext
 
 from utils import get_tokens
 from pytrie import trie
 
-def index_input_files(inv_idx):
+inv_idx = trie()
+
+
+def index_words(words, doc_id, text):
+    for word in words:
+        inv_idx.insert(word, doc_id, words[word])
+
+
+def index_input_files():
     for root, _, files in os.walk('input_files'):
         for file in files:
-            
-            print(f'indexing - {file}')
             file_path = os.path.join(root, file)
-            
             with open(file_path, "rb") as f:
-                text = None
+                tokens = None
                 if file.endswith('.pdf'):
                     text = '\n'.join(pdftotext.PDF(f))
+                    tokens = get_tokens(text, False)
                 elif file.endswith('.txt'):
                     text = f.read().decode()
-            
-            if text is not None:
-                tokens = get_tokens(text)
-                for i, para in enumerate(tokens, 0):
-                    id = f'{file_path}_para_{i}'
-                    for word in para:
-                        count = para[word]
-                        inv_idx.insert(word, id, count)
+                    tokens = get_tokens(text, True)
+
+            if tokens is not None:
+                if file.endswith('.pdf'):
+                    id = file_path
+                    for text, para in tokens:
+                        index_words(para, id, text)
+                elif file.endswith('.txt'):
+                    for i, (text, para) in enumerate(tokens, 0):
+                        id = f'{file_path}_para_{i}'
+                        index_words(para, id, text)
+
 
 if __name__ == '__main__':
     inv_idx = trie()
-    index_input_files(inv_idx)
+
+    index_input_files()
 
     print(inv_idx.get_by_prefix('lorem'))
     print(inv_idx.get_by_prefix('process'))
-    
--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ import os
 import re
 
 
-def get_tokens(text):
+def get_tokens(text, split=True):
     tokens = []
-    paras = text.split('\n\n')
+
+    paras = re.split(r'\n.\n', text) if split else [text]
     for para in paras:
+        para = re.sub(r'[^\x00-\x7F]',' ', para)
         words = re.split(r'[ \n]', para)
         words = [re.sub(r'[^a-z0-9]', '', word.lower()) for word in words]
         words = list(filter(lambda x: len(x) > 0, words))
@@ -13,5 +15,5 @@ def get_tokens(text):
         word_dic = {}
         for word in words:
             word_dic[word] = word_dic.get(word, 0) + 1
-        tokens.append(word_dic)
+        tokens.append((para, word_dic))
     return tokens
