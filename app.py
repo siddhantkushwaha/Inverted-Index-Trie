8785e1493fdc978b349a7c6e6fdd1df4e2aa1b8f
siddhant <k16.siddhant@gmail.com>
2019-11-23 23:32:44 +0530

    did necessary changes and created required endpoints.

--- app.py
+++ app.py
@@ -1,10 +1,73 @@
+import os
+
 from flask import Flask, request, jsonify
+
+from pytrie import trie
+from main import index_file
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def home():
+    return 'Get request to server.'
+
+
+@app.route('/index', methods=['POST'])
 def index():
-    return 'Get request to EAST server.'
+    file_buf = request.files['file']
+    
+    file_name = file_buf.filename
+    index_file(inv_idx, file_buf, file_name)
+
+    with open(f'uploaded_files/{file_name}', 'wb') as f:
+        file_buf.seek(0)
+        f.write(file_buf.read())
+
+    return f'Indexed. {file_name}\n'
+
+@app.route('/word_search', methods=['GET'])
+def word_search():
+    query = request.args.get('query', None)
+    print(query)
+
+    results = []
+    if query is not None:
+        results = inv_idx.search_word(query)
+        results = list(results)
+
+    print(results)
+    return jsonify(results)
+
+@app.route('/prefix_search', methods=['GET'])
+def prefix_search():
+    query = request.args.get('query', None)
+    print(query)
+
+    results = []
+    if query is not None:
+        res = inv_idx.get_by_prefix(query)
+        for word, docs in res:
+            results.append((word, list(docs)))
+
+    print(results)
+    return jsonify(results)
+
+@app.route('/clear', methods=['GET'])
+def clear():
+    # delete the old trie and declare a new one.
+    global inv_idx
+    del inv_idx
+    inv_idx = trie()
+    return 'Index cleared.'
+
 
 if __name__ == '__main__':
+
+    # create folder for uploaded files
+    os.system('mkdir -p uploaded_files')
+
+    # declear an inverted index, uses trie from trie.cpp
+    inv_idx = trie()
+
     app.run(host='0.0.0.0', port=8000)
--- main.py
+++ main.py
@@ -6,42 +6,45 @@ import pdftotext
 from utils import get_tokens
 from pytrie import trie
 
-inv_idx = trie()
 
-
-def index_words(words, doc_id, text):
+def index_words(inv_idx, words, doc_id, text):
     for word in words:
         inv_idx.insert(word, doc_id, words[word])
 
 
-def index_input_files():
+def index_file(inv_idx, file_buf, file_name):
+    tokens = None
+    if file_name.endswith('.pdf'):
+        text = '\n'.join(pdftotext.PDF(file_buf))
+        tokens = get_tokens(text, False)
+    elif file_name.endswith('.txt'):
+        text = file_buf.read().decode()
+        tokens = get_tokens(text, True)
+
+    if tokens is not None:
+        if file_name.endswith('.pdf'):
+            id = file_name
+            for text, para in tokens:
+                index_words(inv_idx, para, id, text)
+        elif file_name.endswith('.txt'):
+            for i, (text, para) in enumerate(tokens, 0):
+                id = f'{file_name}_para_{i}'
+                index_words(inv_idx, para, id, text)
+
+
+def index_input_files(inv_idx):
     for root, _, files in os.walk('input_files'):
         for file in files:
             file_path = os.path.join(root, file)
-            with open(file_path, "rb") as f:
-                tokens = None
-                if file.endswith('.pdf'):
-                    text = '\n'.join(pdftotext.PDF(f))
-                    tokens = get_tokens(text, False)
-                elif file.endswith('.txt'):
-                    text = f.read().decode()
-                    tokens = get_tokens(text, True)
+            print(file_path)
 
-            if tokens is not None:
-                if file.endswith('.pdf'):
-                    id = file_path
-                    for text, para in tokens:
-                        index_words(para, id, text)
-                elif file.endswith('.txt'):
-                    for i, (text, para) in enumerate(tokens, 0):
-                        id = f'{file_path}_para_{i}'
-                        index_words(para, id, text)
+            with open(file_path, 'rb') as f:
+                index_file(inv_idx, f, file_path)
 
 
 if __name__ == '__main__':
     inv_idx = trie()
-
-    index_input_files()
+    index_input_files(inv_idx)
 
     print(inv_idx.get_by_prefix('lorem'))
     print(inv_idx.get_by_prefix('process'))
